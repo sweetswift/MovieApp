1697c8dc1a1bca9c9c7caf055ee79bdb6ab01590
LeeChungWoo <ios.lllccww@gmail.com>
2023-01-04 22:07:39 +0900

    [ADD] MovieCell 추가

---
+++ MovieApp/MovieCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieCell.swift
+//  MovieApp
+//
+//  Created by chungwoolee on 2023/01/03.
+//
+
+import UIKit
+
+
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    
+}

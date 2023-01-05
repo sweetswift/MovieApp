47f943eb0998671b8528ca35e62e134c160cd9ad
LeeChungWoo <ios.lllccww@gmail.com>
2023-01-05 21:32:28 +0900

    [UPDATE] 이미지 받아오는 함수 추가

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     
     var movieModel: MovieModel?
-
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var movieTableView: UITableView!
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,7 +23,30 @@ class ViewController: UIViewController {
         
         requestMovieAPI()
     }
-
+    
+    func loadImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        if let hasURL = URL(string: urlString) {
+            var request = URLRequest(url: hasURL)
+            request.httpMethod = "GET"
+            
+            session.dataTask(with: request) { data, response, error in
+                print((response as! HTTPURLResponse).statusCode)
+                
+                if let hasData = data {
+                    UIImage(data: hasData)
+                    completion(UIImage(data: hasData))
+                    return
+                }
+            }.resume()
+            session.finishTasksAndInvalidate()
+        }
+        
+        completion(nil)
+    }
+    
     // network
     func requestMovieAPI() {
         let sessionConfig = URLSessionConfiguration.default
@@ -40,37 +63,41 @@ class ViewController: UIViewController {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
-       let task = session.dataTask(with: request) { data, response, error in
-           print((response as! HTTPURLResponse).statusCode)
-           
-           if let hasData = data {
-               do {
-                   self.movieModel = try JSONDecoder().decode(MovieModel.self, from: hasData)
-                   print(self.movieModel ?? "no data")
-                   
-                   DispatchQueue.main.async {
-                       self.movieTableView.reloadData()
-                   }
-                   
-                   
-               }catch{
-                   print(error)
-               }
-               
-           }
-           
+        let task = session.dataTask(with: request) { data, response, error in
+            print((response as! HTTPURLResponse).statusCode)
+            
+            if let hasData = data {
+                do {
+                    self.movieModel = try JSONDecoder().decode(MovieModel.self, from: hasData)
+                    print(self.movieModel ?? "no data")
+                    
+                    DispatchQueue.main.async {
+                        self.movieTableView.reloadData()
+                    }
+                    
+                    
+                }catch{
+                    print(error)
+                }
+                
+            }
+            
         }
         
         task.resume()
         session.finishTasksAndInvalidate()
     }
-
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return self.movieModel?.results.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -82,6 +109,17 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let currency = self.movieModel?.results[indexPath.row].currency ?? ""
         let price = self.movieModel?.results[indexPath.row].trackPrice.description ?? ""
         cell.priceLabel.text = currency + price
+        
+        if let hasURL = self.movieModel?.results[indexPath.row].image {
+            self.loadImage(urlString: hasURL) { image in
+                DispatchQueue.main.async {
+                    cell.movieImageView.image = image
+                }
+                
+            }
+        }
+        
+    
         
         return cell
     }
